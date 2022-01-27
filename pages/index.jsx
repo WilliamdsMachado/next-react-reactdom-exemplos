da95863f94636056895ac0b9858449142bb62e09
William Machado <dohillo@gmail.com>
2022-01-27 16:23:44 -0300

    estilizado title e usando children

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,10 +4,10 @@ import Title from '../src/components/title/Title'
 function HomePage () {
   return (
     <div>
-      <Title />
-      <Title />
-      <Title />
-      Hello world, NextJs + ReactJs
+      <Title>Um titulo</Title>
+      <p>Um paragrafo qualquer</p>
+      <Title>Outro Titulo</Title>
+      <p>Um outro paragrafo qualquer</p>
     </div>
   )
 }
