257dd2ac2695e684f2ac879e8e5bbb6e0fd7d3ff
William Machado <dohillo@gmail.com>
2022-01-27 17:27:46 -0300

    Criado stylo global e subtitulo

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,13 @@
 import Title from '../src/components/title/Title'
-
+import Subtitle from '../src/components/subtitle/Subtitle'
 
 function HomePage () {
   return (
     <div>
       <Title>Um titulo</Title>
-      <p>Um paragrafo qualquer</p>
+      <Subtitle text="Um subtitulo"/>
       <Title>Outro Titulo</Title>
-      <p>Um outro paragrafo qualquer</p>
+      <Subtitle text="Um outro Subtitulo"/>
     </div>
   )
 }
---
+++ src/components/subtitle/Subtitle.jsx
@@ -0,0 +1,13 @@
+/*
+    Exemplo de componente react com props(atributos)
+*/
+
+
+function Subtitle ({ text }) {
+    return (
+        <h4>{ text }</h4>
+    )
+}
+
+export default Subtitle
+
