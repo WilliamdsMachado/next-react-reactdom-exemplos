cfc649c6a5a171b3ae5266b6c9e99e9d2c1780f5
William Machado <dohillo@gmail.com>
2022-01-27 18:01:35 -0300

    Impotado font e configuraçoes do titulo

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,12 +3,17 @@ import Subtitle from '../src/components/subtitle/Subtitle'
 
 function HomePage () {
   return (
+    <>
+      <Head>
+        <title>Titulo diferente da home page</title>
+      </Head>
     <div>
       <Title>Um titulo</Title>
       <Subtitle text="Um subtitulo"/>
       <Title>Outro Titulo</Title>
       <Subtitle text="Um outro Subtitulo"/>
     </div>
+    </>
   )
 }
 export default HomePage
